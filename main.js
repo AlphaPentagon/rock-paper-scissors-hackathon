805c77438660dda97574f47c86500cd7276a56d0
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-04-29 15:38:46 +0100

    Completed 2nd bonus task for ticket 7

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function displayOutcome(outcome){
     alert(outcome);
 };
 
-//Calls all needed functions
+//initiates shared variables 
 let gameCount = 0;
 let winCount = 0;
 let loseCount = 0;
@@ -67,16 +67,18 @@ function main(){
     let result;
     let gamePlaying = true;
     
-
+//game loop to call all necessary functions
     while(gamePlaying ===true){
 
-    getplayerName();
+    getPlayerName();
+    capitaliseName();
     playerMove = getPlayerInput();
     computerMove = getComputerInput();
     result = getWinner(playerMove, computerMove);
     displayOutcome(result);
-    gamePlaying = confirmCheck();    
-    }
+    gamePlaying = confirmCheck(); 
+
+    };
 
 };
 
@@ -88,33 +90,63 @@ function getComputerInput() {
     let possibleMoves = ["rock", "paper", "scissors"];   
     let randomNum = Math.floor(Math.random() * 3);    
     return possibleMoves[randomNum];
-}
+
+};
 
 //Ticket 5
 
 //Checks if the player would like to keep playing 
 function confirmCheck(){
     
-    return confirm(`Games played: ${gameCount}\nWins: ${winCount}\nLosses: ${loseCount}\nDraws: ${drawCount}\nDo you wish to play again ${playerName}?`)
-}
+    return confirm(`Games played: ${gameCount}\nWins: ${winCount}\nLosses: ${loseCount}\nDraws: ${drawCount}\nDo you wish to play again ${playerName}?`);
+};
 
 //Ticket 7 
-function getplayerName(){
+//single check for both valid types, did this first kept in just in case
+/*
+function getPlayerName(){
 
-    let approvedFirstLetter = false;
-
-    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    let validName = false;
 
     playerName = prompt("Please input your User Name. This should be no more than 10 characters and must start with a letter");
 
-    
-
+    while(validName === false){
         
-    playerName = prompt("Sorry that is not a valid User Name! The first character of your User Name MUST BE A LETTER!");
+        while(checkFirstLetter() === false || checkNameLength() === false){
+
+            playerName = prompt(`Sorry that is not a valid User Name! The first character of your User Name MUST BE A LETTER AND LESS THAN 10 CHARACTERS!\n\nPlease enter a valid user name`);  
+                     
+        }
+
+        validName = true;
+    };
+};
+*/
+//two seperate checks for unique error message 
+function getPlayerName(){
+
+    let validName = false;
+
+    playerName = prompt("Please input your User Name. This should be no more than 10 characters and must start with a letter");
+
+    while(validName ===false){
+
+        while(checkFirstLetter() === false){
+
+            playerName = prompt(`Sorry that is not a valid User Name! The first character of your User Name MUST BE A LETTER!\n\nPlease enter a valid user name`);
        
-    playerName = prompt("Sorry that is not a valid User Name! Please input a User Name LESS THAN 10 CHARACTERS!");
-    return playerName;
-}
+        };
+        while(checkNameLength() === false){
+
+            playerName = prompt(`Sorry that is not a valid User Name! Please input a User Name LESS THAN 10 CHARACTERS!\n\nPlease enter a valid user name`);
+        
+        };
+        if(checkFirstLetter() === true && checkNameLength() === true){
+            return
+        };
+    };
+};
+
 //checks name length
 function checkNameLength(){
     
@@ -122,23 +154,26 @@ function checkNameLength(){
         return true;
     }
     return false;
-}
+};
 
 //checks first character is a letter
 function checkFirstLetter(){
+
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
     for (let i = 0; i < alphabet.length; i++) {
             
         if (alphabet[i] === playerName[0].toLowerCase()) {           
             return true;                  
-        }   
-    }
+        } ;  
+    };
     return false;
+};
+
+function capitaliseName(){
+
+    playerName = playerName[0].toUpperCase()+ playerName.slice(1);
 }
-
-//Bonus 1
-
-
 
 //runs the program
 main();
